8d5e3eb684d9d52fac8c875a32305b2601213eb7
saloum45 <saloumfall45@gmail.com>
2025-05-11 15:53:10 +0000

    in progress

--- database/migrations/2025_05_11_153658_create_historique_demande_conges_table.php
+++ database/migrations/2025_05_11_153658_create_historique_demande_conges_table.php
@@ -11,21 +11,22 @@ return new class extends Migration
         Schema::create('historique_demande_conges', function (Blueprint $table) {
             $table->id();
             $table->string('niveau_validation');
-$table->string('decision');
-$table->string('commentaire');
-$table->string('date_validation');
-$table->unsignedBigInteger('id_user');
-$table->foreign('id_user')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
-$table->unsignedBigInteger('id_demandeconge');
-$table->foreign('id_demandeconge')->references('id')->on('demandeconges')->onDelete('cascade')->onUpdate('cascade');
+            $table->string('decision');
+            $table->string('commentaire');
+            $table->string('date_validation');
+            $table->unsignedBigInteger('id_user');
+            $table->foreign('id_user')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->unsignedBigInteger('id_demandeconge');
+            $table->foreign('id_demandeconge')->references('id')->on('demandeconges')->onDelete('cascade')->onUpdate('cascade');
 
             $table->timestamps();
-        });    }
+        });
+    }
 
     public function down()
     {
         Schema::dropIfExists('historique_demande_conges');
-        if ('historique_demande_conges' === "Users"||'historique_demande_conges' === "User" ||'historique_demande_conges' === "user"||'historique_demande_conges' === "users") {
+        if ('historique_demande_conges' === "Users" || 'historique_demande_conges' === "User" || 'historique_demande_conges' === "user" || 'historique_demande_conges' === "users") {
             Schema::dropIfExists('password_reset_tokens');
             Schema::dropIfExists('sessions');
         }
