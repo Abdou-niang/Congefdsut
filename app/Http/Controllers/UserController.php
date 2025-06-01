291f0574104ebcaa618bdab2468bd2cda242b121
saloum45 <saloumfall45@gmail.com>
2025-06-01 15:54:04 +0000

    in progress

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,7 +21,7 @@ class UserController extends Controller
     public function index()
     {
         try {
-            $data = User::select('*')->join('utilisateur_privileges', 'id_user', '=', 'users.id')->get();
+            $data = User::select('*')->with('privileges.service','privileges.cellule')->get();
             return $this->successResponse($data, 'Récupération réussie');
         } catch (Exception $e) {
             return $this->errorResponse('Récupération échouée', 500, $e->getMessage());
